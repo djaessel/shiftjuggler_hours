e945e526962b64fa1abb4782c717b3e0d51a8bd5
Johannes Koch <j.koch@taleoo.de>
2025-04-16 00:14:33 +0200

    fixed saturday and sunday no work days to count + version change + console.log removal

--- hours_handler.js
+++ hours_handler.js
@@ -19,24 +19,28 @@ function showHoursInfo(request, sender, sendResponse) {
         let finalHoursTxt = resultData.children[5].innerHTML;
         let finalHours = parseFloat(finalHoursTxt.replace(',', '.'));
         totalHours += finalHours;
-        workdays += 1;
+
+        let mySubmittedData = tableRows[i].children[0];
+        let dayInfoText = mySubmittedData.children[0].innerHTML;
+        if (!(dayInfoText.includes(" - Sa") || dayInfoText.includes(" - So"))) {
+          workdays += 1;
+        }
       }
     } else if (tableRows[i].children.length == 1) {
       let resultData = tableRows[i].children[0];
       if (resultData.children.length > 5) {
         let infoText = resultData.children[8].innerHTML;
         if (infoText.includes("gemeldet")) {
-          for (var xyy = 0; xyy < resultData.children.length; xyy++) {
-            console.log(resultData.children[xyy].innerHTML)
-          }
           let finalHoursTxt = resultData.children[7].innerHTML;
           finalHoursTxt = finalHoursTxt.replace('<span>', '')
           finalHoursTxt = finalHoursTxt.replace('</span>', '')
           let finalHours = parseFloat(finalHoursTxt.replace(',', '.'));
-          console.log("FINAL_HOURS:")
-          console.log(finalHours)
           totalHours += finalHours;
-          workdays += 1;
+
+          let dayInfoText = resultData.children[0].innerHTML;
+          if (!(dayInfoText.includes(" - Sa") || dayInfoText.includes(" - So"))) {
+            workdays += 1;
+          }
         }
       }
     }
